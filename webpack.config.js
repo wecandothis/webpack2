b541809e34c14d9e942c21df88ba455a3db612f0
feng <490884308@qq.com>
2017-06-12 09:33:33 +0800

    第四次提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 var path=require('path');// 导入路径包 
 var webpack=require('webpack'); 
-module.exports={ 
-entry:[//入口文件 
+//注入环境变量，来判断生产环境还是开发环境
+var DEVELOPMENT=process.env.NODE_ENV==='development'; 
+var PRODUCTION=process.env.NODE_ENV==='production'; 
+var entry=PRODUCTION 
+? ['./src/index.js'] : [ 
 './src/index.js', 
-'webpack/hot/dev-server',//调用热重载 hot 
+'webpack/hot/dev-server',//开启热重载 hot 
 'webpack-dev-server/client?http://localhost:8080'//添加webpack-dev-server客户端 
-], 
-plugins:[ 
-new webpack.HotModuleReplacementPlugin()//全局开启热代码替换 
-], 
+]; 
+var plugins=PRODUCTION ? [] : [ 
+new webpack.HotModuleReplacementPlugin()//全局开启代码热替换 如果是CLI这里则不用写 
+]; 
+
+module.exports={ 
+entry:entry//入口文件 
+, 
+plugins:plugins 
+, 
 output:{ 
 path:path.join(__dirname,'dist'),// 指定打包之后的文件夹 
 publicPath:'/dist/',// 指定资源文件引用的目录 
